bfd6e02661cfc6cdac06db8afeeb233a756ca784
gproduct <gproduct1379@yahoo.com>
2017-08-02 18:14:42 +0200

    Added key comments for better understanding of the code

--- arduino_framework/sender/sender.ino
+++ arduino_framework/sender/sender.ino
@@ -3,26 +3,30 @@
 #include <TinyGPS.h>
 #include <SoftwareSerial.h>
 
-float flat, flon, alti;
-unsigned long age;
-int satellites;
+float flat, flon, alti;                           // latitude, longitude and altitude for gps
+unsigned long age;                                // age for gps
+int satellites;                                   // num of satellites
 
 TinyGPS gps;
 SoftwareSerial ss(3, 4); // Arduino RX, TX , 
 
 void setup() {
   
-  Serial.begin(9600);  // Serial to print out GPS info in Arduino IDE
-  ss.begin(9600); // SoftSerial port to get GPS data. 
+  Serial.begin(9600);                             // Serial to print out in Arduino IDE
+  ss.begin(9600);                                 // SoftSerial port to get GPS data. 
 
   if (!LoRa.begin(866E6)) {                       // starting LoRa at 868Mhz
     Serial.println("Starting LoRa failed!");      // did not start 
     while (1);                                    // put debug here, led or display
   }
-
-   smartdelay(2000);
+  
+   smartdelay(2000);                              // wait for gps to stabalize
 }
 
+/*
+ *  Function: static void smartdelay(unsigned long ms)
+ *  Description: waits a certain time defined by the ms and reads the gps
+ */
 static void smartdelay(unsigned long ms)
 {
   unsigned long start = millis();
@@ -37,64 +41,47 @@ static void smartdelay(unsigned long ms)
 }
 
 void loop() {
-    satellites = gps.satellites();
-    
+    satellites = gps.satellites();                  // gets the num of satellites
+
+    // debug stuff
     Serial.print(satellites);
     Serial.print("-");
     
-    if(satellites != 0) {
-      gps.f_get_position(&flat, &flon, &age);
-      
-      Serial.print(flat);
+    if(satellites != 0) {                           // if the satellites are not 0
+      gps.f_get_position(&flat, &flon, &age);       // get position
+
+      // debug stuff
+      Serial.print(flat, 5);
       Serial.print("-");
-      Serial.print(flon);
+      Serial.print(flon, 5);
       
-      alti = gps.f_altitude();
+      alti = gps.f_altitude();                      // get altitude
   
-      send_gps(flat, flon, alti);
+      send_gps(flat, flon, alti);                   // send with lora
     } else {
+      // whoops, error!
       Serial.print("Nothing to see here");
       
-      lora_send_string("nothing");
+      lora_send_string("nothing");  
     }
     Serial.println();
-    smartdelay(1000);
+    smartdelay(1000);                               // smarty delay
     
 }
 
-
+/*
+ *  Function: void send_gps(float lat, float lon, float alti)
+ *  Description: send lat, lon and alti through lora
+ */
 void send_gps(float lat, float lon, float alti) {
-  LoRa.beginPacket();
-  LoRa.print('a');
-  LoRa.print(lat * 10000);
-  LoRa.print('o');
-  LoRa.print(lon * 10000);
-  LoRa.print('t');
-  LoRa.print(alti * 10000);
-  LoRa.endPacket();
-}
-
-/*
- *  Function:    void lora_send_num(size_t data)
- *  Parameters:  size_t data -> a number 
- *  Description: Sends a integer or a number through the LoRa system
- */
-void lora_send_num(size_t data) {
-  LoRa.beginPacket();
-  LoRa.print(data);
-  LoRa.endPacket();
-}
-
-/*
- *  Function:    void lora_send_float(float data)
- *  Parameters:  float data -> float 
- *  Description: Sends a float through the LoRa system
- *  Problems: it will not send 1.23456 it will send 1.23
- */
-void lora_send_float(float data) {
-  LoRa.beginPacket();
-  LoRa.print(data);
-  LoRa.endPacket();
+  LoRa.beginPacket();                               // begin packet
+  LoRa.print('a');                                  // to spot lat beginning
+  LoRa.print(lat * 10000);                          // multiply for easier transfer
+  LoRa.print('o');                                  // to spot lon beginning
+  LoRa.print(lon * 10000);                          // multiply for easier transfer
+  LoRa.print('t');                                  // to spot alti beginning
+  LoRa.print(alti * 10000);                         // multiply for easier transfer
+  LoRa.endPacket();                                 // end packet
 }
 
 /*
