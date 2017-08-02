eff4ee7373422dbba1efa3a6f6b755208c127c90
gproduct <gproduct1379@yahoo.com>
2017-08-02 13:05:23 +0200

    Testing with real data

--- sender/sender.ino
+++ sender/sender.ino
@@ -21,17 +21,8 @@ void setup() {
 
 
 void loop() {
-  /*
-   * LoRa sending data, fake data
-   */
-  float flat, flon;
-  //45.248618, 19.840049
-  flat = 45.248618; flon = 19.840049; 
-  send_gps(flat, flon);
-
-  delay(5000);
   
-  /*
+  
   bool newData = false;
   unsigned long chars;
   unsigned short sentences, failed;
@@ -52,26 +43,8 @@ void loop() {
     float flat, flon;
     unsigned long age;
     gps.f_get_position(&flat, &flon, &age);
-    Serial.print("LAT=");
-    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-    Serial.print(" LON=");
-    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
-    Serial.print(" SAT=");
-    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
-    Serial.print(" PREC=");
-    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+    send_gps(flat, flon);
   }
-  
-  gps.stats(&chars, &sentences, &failed);
-  Serial.print(" CHARS=");
-  Serial.print(chars);
-  Serial.print(" SENTENCES=");
-  Serial.print(sentences);
-  Serial.print(" CSUM ERR=");
-  Serial.println(failed);
-  if (chars == 0)
-    Serial.println("** No characters received from GPS: check wiring **");
-  */
 }
 
 void send_gps(float lat, float lon) {
