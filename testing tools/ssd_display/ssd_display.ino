5714e09f1d7d0c944ad5bb205a716e1eb3fe5b25
gproduct <gproduct1379@yahoo.com>
2017-08-02 10:56:48 +0200

    Added display for easy debug and to see what is being received

--- testing tools/ssd_display/ssd_display.ino
+++ testing tools/ssd_display/ssd_display.ino
@@ -24,31 +24,19 @@ All text above, and the splash screen must be included in any redistribution
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-
-#if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
 
 void setup()   {                
   Serial.begin(9600);
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  display.begin(SSD1306_SWITCHCAPVCC, 0x78);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
   // init done
   
 
   // Clear the buffer.
   display.clearDisplay();
   display.setTextSize(1);
-  display.setTextColor(WHITE);
+ // display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.println("Hello, world!");
   display.display();
@@ -58,6 +46,5 @@ void setup()   {
 
 void loop() {
 
-}
 }
 
