92974558acabee6efbb2f816cabade19fe9fca85
gproduct <gproduct1379@yahoo.com>
2017-08-03 08:31:06 +0200

    Altitude fixup with the buffer (>>)

--- arduino_framework/receiver/receiver.ino
+++ arduino_framework/receiver/receiver.ino
@@ -6,8 +6,7 @@
 #include <Wire.h>
 #endif
 
-U8X8_SSD1306_128X32_UNIVISION_SW_I2C u8x8(SCL, SDA, U8X8_PIN_NONE);                             // setup the fancy display
-
+U8X8_SSD1306_128X32_UNIVISION_SW_I2C u8x8(SCL, SDA, U8X8_PIN_NONE);                             // setup the fancy displaysnr
 
 void setup() {
   Serial.begin(9600);                                                                           // serial for debugging
@@ -29,7 +28,6 @@ void setup() {
 }
 
 void loop() {
- 
 }
 
 /*
@@ -37,9 +35,9 @@ void loop() {
  *  Description: execute when we receive a packet on LoRa. Process it and update the display
  */
 void onReceive(int packetSize) {
-  char data[packetSize];                                                                        // packet data
+  char data[packetSize];                                                                        // packet data           
   float lat, lon, alti;                                                                         // latitude, longitude and altitude
-  int rssi; float snr;                                                                          // rssi and snr
+  int rssi; float snr;                                                                          // rssi and                                                             
   
   for (int i = 0; i < packetSize; i++) {                  
     data[i] =  ((char)LoRa.read());                                                             // put the data into the buffer
@@ -54,7 +52,7 @@ void onReceive(int packetSize) {
     
     for(int i=1;i<10;i++)           { lat_char[i - 1]   = data[i];}                             // write it into the local arrays
     for(int i=11;i<20;i++)          { lon_char[i - 11]  = data[i];}                             // write it into the local arrays
-    for(int i=20;i<packetSize; i++) { alti_char[i - 20] = data[i]; }                            // write into the local arrays
+    for(int i=21;i<packetSize; i++) { alti_char[i - 21] = data[i]; }                            // write into the local arrays
 
     lat = (float)atof(lat_char); lon = (float)atof(lon_char); alti = (float)atof(alti_char);    // convert it to float
 
@@ -63,12 +61,17 @@ void onReceive(int packetSize) {
     // debug printing with 10 decimal points
     Serial.println(lat, 10);                                        
     Serial.println(lon, 10);
+    Serial.println(alti, 10);
+    /*for(int i=0; i < packetSize; i++ ){                                                         // debug received data
+      Serial.print(data[i]);
+    }
+    Serial.println();*/
 
     
     rssi = LoRa.packetRssi();                                                                   // get rssi 
     snr  = LoRa.packetSnr();                                                                    // get snr
     
-    update_display(lat, lon, rssi, snr);                                                        // update the display
+    update_display(lat, lon, alti, rssi, snr);                                                        // update the display
     
   } else {
     u8x8.clear();                                                                               // clear the display
@@ -78,6 +81,7 @@ void onReceive(int packetSize) {
     for(int i=0; i < packetSize; i++ ){                                                         // debug received data
       Serial.print(data[i]);
     }
+    Serial.println();
   }
 }
 
@@ -85,19 +89,19 @@ void onReceive(int packetSize) {
  *  Function: void update_display(char* data, int rssi, float snr)
  *  Description: updates the display with the received data, rssi and snr. If using buffer than String data must be char* data
  */
-void update_display(float lat, float lon, int rssi, float snr) {
+void update_display(float lat, float lon, float alti, int rssi, float snr) {
   
   u8x8.setFont(u8x8_font_chroma48medium8_r);                                                    // set font                                                                       
   u8x8.clear();                                                                                 // clear the display
   
   u8x8.setCursor(0,0);                                                                          // set cursor on the first row
-  u8x8.print("Lat: "); u8x8.print(lat, 5);                                                      // print latitude
+  u8x8.print("Lat: "); u8x8.print(lat, 3);                                                      // print latitude
     
   u8x8.setCursor(0,1);                                                                          // set cursor on the second row
-  u8x8.print("Lon: "); u8x8.print(lon, 5);                                                      // print longitude
+  u8x8.print("Lon: "); u8x8.print(lon, 3);                                                      // print longitude
   
   u8x8.setCursor(0,2);                                                                          // set cursor on the third row
-  u8x8.print("Alti: "); u8x8.print(alti, 5);                                                    // print altitude
+  u8x8.print("Alt: "); u8x8.print(alti, 3);                                                    // print altitude
   
   u8x8.setFont(u8x8_font_5x7_f);                                                                // set smaller font
   
